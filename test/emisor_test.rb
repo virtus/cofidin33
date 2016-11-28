33afbb40b7674e5d5ceafb9e338cea0c233855d6
Hector Sansores <hector.sansores@virtus.com.mx>
2016-11-27 19:11:38 -0600

    Pruebas de Emisor

--- test/emisor_test.rb
+++ test/emisor_test.rb
@@ -7,20 +7,54 @@ class EmisorTest < Minitest::Spec
       nombre: 'Virtus Consultores, S.A. de C.V.'
     }
 
+    domicilio_emisor = {
+      calle: 'Valle de Solís',
+      no_exterior: '33',
+      colonia: 'El Mirador',
+      codigo_postal: '53050',
+      municipio: 'Naucalpan',
+      estado: 'México',
+      pais: 'México'
+    }
+
     @comprobante = Cofidin::Comprobante.new
     @comprobante.emisor.atributos = emisor
+    @comprobante.emisor.domicilio_fiscal.atributos = domicilio_emisor
     xml = Cofidin::Serializer.new.serialize @comprobante
     doc = Nokogiri::XML(xml)
     @emisor = doc.at_css "cfdi|Emisor"
+    @domicilio_fiscal = doc.at_css 'cfdi|DomicilioFiscal'
   end
 
-  it 'el Emisor tiene un atributo rfc' do
-    @emisor.attributes['rfc'].name.must_equal 'rfc'
-    @emisor.attributes['rfc'].value.must_equal 'VCO980224GM7'
+  describe 'Emisor' do
+    it 'tiene un atributo rfc' do
+      @emisor.attributes['rfc'].value.must_equal 'VCO980224GM7'
+    end
+
+    it 'tiene un atributo nombre' do
+      @emisor.attributes['nombre'].value.must_equal 'Virtus Consultores, S.A. de C.V.'
+    end
   end
 
-  it 'el Emisor tiene un atributo nombre' do
-    @emisor.attributes['nombre'].name.must_equal 'nombre'
-    @emisor.attributes['nombre'].value.must_equal 'Virtus Consultores, S.A. de C.V.'
+  describe 'DomicilioFiscal' do
+    it 'tiene un atributo calle' do
+      @domicilio_fiscal.attributes['calle'].value.must_equal 'Valle de Solís'
+    end
+
+    it 'tiene un atributo municipio' do
+      @domicilio_fiscal.attributes['municipio'].value.must_equal 'Naucalpan'
+    end
+
+    it 'tiene un atributo estado' do
+      @domicilio_fiscal.attributes['estado'].value.must_equal 'México'
+    end
+
+    it 'tiene un atributo país' do
+      @domicilio_fiscal.attributes['pais'].value.must_equal 'México'
+    end
+
+    it 'tiene un atributo codigoPostal' do
+      @domicilio_fiscal.attributes['codigoPostal'].value.must_equal '53050'
+    end
   end
 end
