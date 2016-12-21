a69d25f75ccf580b0ef43ce44d420e8658e3e62a
Hector Sansores <hector.sansores@virtus.com.mx>
2016-12-20 20:08:13 -0600

    Mueve los elementos a su folder

--- lib/cofidin.rb
+++ lib/cofidin.rb
@@ -1,18 +1,17 @@
 require "cofidin/version"
-require "cofidin/comprobante"
-require "cofidin/emisor"
-require "cofidin/receptor"
-require "cofidin/ubicacion"
-require "cofidin/domicilio_fiscal"
-require "cofidin/concepto"
-require "cofidin/impuestos"
-require "cofidin/retencion"
-require "cofidin/traslado"
+require "cofidin/elementos/comprobante"
+require "cofidin/elementos/emisor"
+require "cofidin/elementos/receptor"
+require "cofidin/elementos/ubicacion"
+require "cofidin/elementos/domicilio_fiscal"
+require "cofidin/elementos/concepto"
+require "cofidin/elementos/impuestos"
+require "cofidin/elementos/retencion"
+require "cofidin/elementos/traslado"
 require "cofidin/serializer"
 require "cofidin/cadena_original"
 require "cofidin/sello_digital"
 require "cofidin/aplica_certificado"
 
 module Cofidin
-  # Your code goes here...
 end
