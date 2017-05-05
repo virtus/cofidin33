9030490b93428294699a00d2286a009a6740655c
Hector Sansores <hector.sansores@virtus.com.mx>
2017-05-04 21:09:39 -0500

    Actualiza los campos del elemento Receptor

--- lib/cofidin/elementos/receptor.rb
+++ lib/cofidin/elementos/receptor.rb
@@ -2,28 +2,35 @@ module Cofidin
   class Receptor
     attr_accessor :rfc
     attr_accessor :nombre
+    attr_accessor :residencia_fiscal
+    attr_accessor :num_reg_id_trib
+    attr_accessor :uso_cfdi
 
     def atributos_sat
       datos = {}
-      datos[:rfc] = rfc
-      datos[:nombre] = nombre if nombre
+      datos[:Rfc] = rfc
+      datos[:Nombre] = nombre if nombre
+      datos[:ResidenciaFiscal] = residencia_fiscal if residencia_fiscal
+      datos[:NumRegIdTrib] = num_reg_id_trib if num_reg_id_trib
+      datos[:UsoCFDI] = uso_cfdi if uso_cfdi
       datos
-    end
-
-    def atributos_sat=(params)
-      self.rfc = params[:rfc]
-      self.nombre = params[:nombre]
     end
 
     def from_hash(params)
       @rfc = params[:rfc]
       @nombre = params[:nombre]
+      @residencia_fiscal = params[:residencia_fiscal]
+      @num_reg_id_trib = params[:num_reg_id_trib]
+      @uso_cfdi = params[:uso_cfdi]
     end
 
     def cadena_original
       campos_cadena = [
         :rfc,
-        :nombre
+        :nombre,
+        :residencia_fiscal,
+        :num_reg_id_trib,
+        :uso_cfdi
       ]
 
       campos_con_datos = campos_cadena.select { |campo| self.send(campo) }
