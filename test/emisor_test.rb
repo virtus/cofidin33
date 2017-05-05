6e1b7960f0d695e3c5b5a657eba8617bf3fc5d55
Hector Sansores <hector.sansores@virtus.com.mx>
2017-05-04 21:09:06 -0500

    Actualiza los campos del elemento Emisor

--- test/emisor_test.rb
+++ test/emisor_test.rb
@@ -2,36 +2,19 @@ require 'test_helper'
 
 class EmisorTest < Minitest::Spec
   before do
-    @atributos_emisor = {
+    @datos_emisor = {
       rfc: 'VCO980224GM7',
-      nombre: 'Virtus Consultores, S.A. de C.V.'
+      nombre: 'Virtus Consultores, S.A. de C.V.',
+      regimen_fiscal: 'Régimen general de personas morales'
     }
-
-    @comprobante = Cofidin::Comprobante.new
-    @comprobante.emisor.atributos_sat = @atributos_emisor
-    xml = Cofidin::GeneraXml.call @comprobante
-    doc = Nokogiri::XML(xml)
-    @emisor = doc.at_css "cfdi|Emisor"
-    @domicilio_fiscal = doc.at_css 'cfdi|DomicilioFiscal'
-  end
-
-  describe 'Emisor' do
-    it 'tiene un atributo rfc' do
-      @emisor.attributes['rfc'].value.must_equal 'VCO980224GM7'
-    end
-
-    it 'tiene un atributo nombre' do
-      @emisor.attributes['nombre'].value.must_equal 'Virtus Consultores, S.A. de C.V.'
-    end
-  end
   end
 
   describe 'cadena original' do
     it 'genera' do
       emisor = Cofidin::Emisor.new
-      emisor.atributos_sat = @atributos_emisor
+      emisor.from_hash @datos_emisor
       cadena = emisor.cadena_original
-      cadena.must_equal 'VCO980224GM7|Virtus Consultores, S.A. de C.V.'
+      cadena.must_equal 'VCO980224GM7|Virtus Consultores, S.A. de C.V.|Régimen general de personas morales'
     end
   end
 end
