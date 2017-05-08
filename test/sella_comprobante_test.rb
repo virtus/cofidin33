96f099f9d73731f6a262ea0bcbfd29c0737b478c
Hector Sansores <hector.sansores@virtus.com.mx>
2017-05-08 17:39:53 -0500

    Corrige los nombres de los atributos

--- test/sella_comprobante_test.rb
+++ test/sella_comprobante_test.rb
@@ -18,9 +18,9 @@ class SellaComprobanteTest < Minitest::Spec
     xml = Cofidin33::SellaComprobante.call @comprobante, certificado, llave_privada, fecha_hora
     doc = Nokogiri::XML(xml)
     node = doc.at_css "cfdi|Comprobante"
-    node.attribute('fecha').value.must_equal fecha_hora
-    node.attribute('noCertificado').value.must_equal '20001000000200001428'
-    node.attribute('certificado').value.wont_equal ''
-    node.attribute('sello').value.wont_equal ''
+    node.attribute('Fecha').value.must_equal fecha_hora
+    node.attribute('NoCertificado').value.must_equal '20001000000200001428'
+    node.attribute('Certificado').value.wont_equal ''
+    node.attribute('Sello').value.wont_equal ''
   end
 end
