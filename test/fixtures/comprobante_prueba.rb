82d6a0c54f3951043af663466a0e6bfd706d751d
Hector Sansores <hector.sansores@virtus.com.mx>
2017-05-08 17:14:46 -0500

    Actualiza comprobante de prueba

--- test/fixtures/comprobante_prueba.rb
+++ test/fixtures/comprobante_prueba.rb
@@ -2,72 +2,77 @@ COMPROBANTE_PRUEBA = {
   serie: 'B',
   folio: '1',
   fecha: '2016-12-15T17:31:46',
-  forma_de_pago: 'Pago en una sola exhibición',
+  forma_pago: '03',
   no_certificado: '12345678901234567890',
   sub_total: '10.35',
+  moneda: 'MXN',
   total: '12.00',
-  tipo_de_comprobante: 'ingreso',
+  tipo_de_comprobante: 'I',
   metodo_de_pago: '03',
-  lugar_expedicion: 'Naucalpan, Estado de México',
+  lugar_expedicion: '53100',
 
   emisor: {
     rfc: 'VCO980224GM7',
     nombre: 'Virtus Consultores, S.A. de C.V.',
-    domicilio_fiscal: {
-      calle: 'Valle de Solís',
-      no_exterior: '33',
-      colonia: 'El Mirador',
-      codigo_postal: '53050',
-      municipio: 'Naucalpan',
-      estado: 'México',
-      pais: 'México'
-    },
-    regimen_fiscal: {
-      regimen: 'Régimen general de ley de personal morales'
-    }
+    regimen_fiscal: '601'
   },
 
   receptor: {
     rfc: 'CRM6702109K6',
     nombre: 'Cruz Roja Mexicana, I.A.P.',
-    domicilio: {
-      calle: 'Juan Luis Vives',
-      no_exterior: '2002',
-      no_interior: 'A',
-      colonia: 'Los Morales Polanco',
-      codigo_postal: '11510',
-      municipio: 'Miguel Hidalgo',
-      estado: 'Ciudad de México',
-      pais: 'México'
-    }
+    uso_cfdi: 'G01'
+
   },
 
   conceptos: [
     {
+      clave_prod_serv: '43231500',
       cantidad: '1',
-      unidad: 'PIEZA',
-      no_identificacion: 'ABC',
+      unidad: 'H87',
       descripcion: 'Concepto 1',
       valor_unitario: '1.23',
-      importe: '1.23'
+      importe: '1.23',
+      impuestos: [
+        traslados: [
+          {
+            base: '1.23',
+            impuesto: '002',
+            tipo_factor: 'Tasa',
+            tasa_o_cuota: '0.160000',
+            importe: '0.1968'
+          }
+        ]
+      ],
     },
     {
+      clave_prod_serv: 81111504,
       cantidad: '2',
-      unidad: 'PIEZA',
-      no_identificacion: 'DEF',
+      unidad: 'H87',
       descripcion: 'Concepto 2',
       valor_unitario: '4.56',
-      importe: '9.12'
+      importe: '9.12',
+      impuestos: [
+        traslados: [
+          {
+            base: '9.12',
+            impuesto: '002',
+            tipo_factor: 'Tasa',
+            tasa_o_cuota: '0.160000',
+            importe: '1.4592'
+          }
+        ]
+      ]
     }
   ],
 
   impuestos: {
-    total_impuestos_trasladados: '1.65',
+    total_impuestos_trasladados: '1.656',
     traslados: [
       {
-        impuesto: 'IVA',
-        tasa: '16.00',
-        importe: '1.65'
+        impuesto: '002',
+        tipo_factor: 'Tasa',
+        tasa_o_cuota: '0.1600',
+        importe: '1.656'
       }
     ]
   }
