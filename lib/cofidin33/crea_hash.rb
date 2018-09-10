44a0dcd93f23f3ca83d810df6871a1f6f020dbe0
Hector Sansores <hector.sansores@virtus.com.mx>
2018-09-10 18:36:25 -0500

    Procesa XML de recibo de pago

--- lib/cofidin33/crea_hash.rb
+++ lib/cofidin33/crea_hash.rb
@@ -72,67 +72,117 @@ module Cofidin33
         end
       end
 
-      conceptos_xml.each do |concepto_xml|
-        concepto = {
-          clave_prod_serv: concepto_xml['ClaveProdServ'],
-          no_identificacion: concepto_xml['NoIdentificacion'],
-          cantidad: concepto_xml['Cantidad'],
-          clave_unidad: concepto_xml['ClaveUnidad'],
-          unidad: concepto_xml['Unidad'],
-          descripcion: concepto_xml['Descripcion'],
-          valor_unitario: concepto_xml['ValorUnitario'],
-          importe: concepto_xml['Importe'],
-          descuento: concepto_xml['Descuento'],
-          impuestos: {
-            traslados: [],
-            retenciones: []
+      if invoice_hash[:tipo_de_comprobante] == 'P'
+        conceptos_xml.each do |concepto_xml|
+          concepto = {
+            clave_prod_serv: concepto_xml['ClaveProdServ'],
+            no_identificacion: concepto_xml['NoIdentificacion'],
+            cantidad: concepto_xml['Cantidad'],
+            clave_unidad: concepto_xml['ClaveUnidad'],
+            unidad: concepto_xml['Unidad'],
+            descripcion: concepto_xml['Descripcion'],
+            valor_unitario: concepto_xml['ValorUnitario'],
+            importe: concepto_xml['Importe'],
+            descuento: concepto_xml['Descuento']
           }
-        }
-
-        traslados_xml = concepto_xml.css('cfdi|Impuestos > cfdi|Traslados > cfdi|Traslado')
-        unless traslados_xml.nil?
-          traslados_xml.each do |traslado_xml|
-            concepto[:impuestos][:traslados] << {
-              base: traslado_xml['Base'],
-              impuesto: traslado_xml['Impuesto'],
-              tipo_factor: traslado_xml['TipoFactor'],
-              tasa_cuota: traslado_xml['TasaOCuota'],
-              importe: traslado_xml['Importe']
+          invoice_hash[:conceptos] << concepto
+        end
+      else
+        conceptos_xml.each do |concepto_xml|
+          concepto = {
+            clave_prod_serv: concepto_xml['ClaveProdServ'],
+            no_identificacion: concepto_xml['NoIdentificacion'],
+            cantidad: concepto_xml['Cantidad'],
+            clave_unidad: concepto_xml['ClaveUnidad'],
+            unidad: concepto_xml['Unidad'],
+            descripcion: concepto_xml['Descripcion'],
+            valor_unitario: concepto_xml['ValorUnitario'],
+            importe: concepto_xml['Importe'],
+            descuento: concepto_xml['Descuento'],
+            impuestos: {
+              traslados: [],
+              retenciones: []
             }
+          }
+
+          traslados_xml = concepto_xml.css('cfdi|Impuestos > cfdi|Traslados > cfdi|Traslado')
+          unless traslados_xml.nil?
+            traslados_xml.each do |traslado_xml|
+              concepto[:impuestos][:traslados] << {
+                base: traslado_xml['Base'],
+                impuesto: traslado_xml['Impuesto'],
+                tipo_factor: traslado_xml['TipoFactor'],
+                tasa_cuota: traslado_xml['TasaOCuota'],
+                importe: traslado_xml['Importe']
+              }
+            end
           end
+
+          retenciones_xml = concepto_xml.css('cfdi|Impuestos > cfdi|Retenciones > cfdi|Retencion')
+          unless retenciones_xml.nil?
+            retenciones_xml.each do |retencion_xml|
+              concepto[:impuestos][:retenciones] << {
+                base: retencion_xml['Base'],
+                impuesto: retencion_xml['Impuesto'],
+                tipo_factor: retencion_xml['TipoFactor'],
+                tasa_cuota: retencion_xml['TasaOCuota'],
+                importe: retencion_xml['Importe']
+              }
+            end
+          end
+          invoice_hash[:conceptos] << concepto
         end
 
-        retenciones_xml = concepto_xml.css('cfdi|Impuestos > cfdi|Retenciones > cfdi|Retencion')
-        unless retenciones_xml.nil?
-          retenciones_xml.each do |retencion_xml|
-            concepto[:impuestos][:retenciones] << {
-              base: retencion_xml['Base'],
-              impuesto: retencion_xml['Impuesto'],
-              tipo_factor: retencion_xml['TipoFactor'],
-              tasa_cuota: retencion_xml['TasaOCuota'],
-              importe: retencion_xml['Importe']
-            }
-          end
+        retenciones_xml.each do |retencion_xml|
+          invoice_hash[:impuestos][:retenciones] << {
+            tipo: 'RETENCION',
+            impuesto: retencion_xml['Impuesto'],
+            importe: retencion_xml['Importe']
+          }
         end
-        invoice_hash[:conceptos] << concepto
+
+        traslados_xml.each do |traslado_xml|
+          invoice_hash[:impuestos][:traslados] << {
+            tipo: 'TRASLADO',
+            impuesto: traslado_xml['Impuesto'],
+            tipo_factor: traslado_xml['TipoFactor'],
+            tasa_cuota: traslado_xml['TasaOCuota'],
+            importe: traslado_xml['Importe']
+          }
+        end
       end
 
-      retenciones_xml.each do |retencion_xml|
-        invoice_hash[:impuestos][:retenciones] << {
-          tipo: 'RETENCION',
-          impuesto: retencion_xml['Impuesto'],
-          importe: retencion_xml['Importe']
+      if invoice_hash[:tipo_de_comprobante] == 'P'
+        pagos_xml = complemento.xpath('pago:Pagos', 'pago' => "http://www.sat.gob.mx/Pagos").first
+        pagos = {
+          version: pagos_xml['Version'],
+          pago: []
         }
-      end
-
-      traslados_xml.each do |traslado_xml|
-        invoice_hash[:impuestos][:traslados] << {
-          tipo: 'TRASLADO',
-          impuesto: traslado_xml['Impuesto'],
-          tipo_factor: traslado_xml['TipoFactor'],
-          tasa_cuota: traslado_xml['TasaOCuota'],
-          importe: traslado_xml['Importe']
-        }
+        pagos_xml.element_children.each do |pago_xml|
+          pago = {
+            fecha_pago: pago_xml[:FechaPago],
+            forma_pago: pago_xml[:FormaDePagoP],
+            moneda: pago_xml[:MonedaP],
+            monto: pago_xml[:Monto],
+            docto_relacionado: []
+          }
+          pago_xml.element_children.each do |docto_xml|
+            docto = {
+              id_documento: docto_xml[:IdDocumento],
+              serie: docto_xml[:Serie],
+              folio: docto_xml[:Folio],
+              moneda: docto_xml[:MonedaDR],
+              metodo_pago: docto_xml[:MetodoDePagoDR],
+              num_parcialidad: docto_xml[:NumParcialidad],
+              imp_saldo_ant: docto_xml[:ImpSaldoAnt],
+              imp_pagado: docto_xml[:ImpPagado],
+              imp_saldo_insoluto: docto_xml[:ImpSaldoInsoluto]
+            }
+            pago[:docto_relacionado] << docto
+          end
+          pagos[:pago] << pago
+        end
+        invoice_hash[:complemento][:pagos] = pagos
       end
 
       [:ok, invoice_hash]
